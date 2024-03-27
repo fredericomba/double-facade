861f66e51cd7cc39ef8db0e70139541e4db41bbf
Frederico Alves <fredericomba.freelancer@gmail.com>
2024-03-27 18:12:18 -0300

    Fix code to pass tests describing the "DoubleFacade" module

--- index.mjs
+++ index.mjs
@@ -1,7 +1,16 @@
-export const create = () => {};
+export const create = () => {
+
+    const requestListener = (request, response) => {};
+
+    return Object.freeze({
+        requestListener,
+    });
+
+};
 
 const DoubleFacade = Object.freeze({
     create,
 });
 
 export default DoubleFacade;
+
