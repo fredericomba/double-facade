17db403599617d871726b82d576c5f65175b7c56
Frederico Alves <fredericomba.freelancer@gmail.com>
2024-03-27 18:08:22 -0300

    Add tests for describing the "DoubleFacade" module

--- tests/index.mjs
+++ tests/index.mjs
@@ -1,4 +1,5 @@
 import assert from 'assert/strict';
+import http from 'http';
 import test from 'node:test';
 
 test('DoubleFacade', async () => {
@@ -8,6 +9,23 @@ test('DoubleFacade', async () => {
     await test('should have a "create" function', async () => {
 
         assert(typeof DoubleFacade.create === 'function');
+
+        const doubleFacade = DoubleFacade.create();
+
+        await test('should be an object with properties', async () => {
+
+            assert(typeof doubleFacade === 'object');
+            assert(doubleFacade !== null);
+
+        });
+
+        await test('should have a "requestListener" for servers', async () => {
+
+            const { requestListener } = doubleFacade;
+
+            assert(typeof requestListener === 'function');
+
+        });
 
     });
 
