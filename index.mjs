46d62c33308b5a9c5ca5c76b6efcd836c3582c2b
Frederico Alves <fredericomba.freelancer@gmail.com>
2024-03-28 14:45:02 -0300

    Fix code to pass tests describing the "DoubleFacade" module

--- index.mjs
+++ index.mjs
@@ -1,6 +1,29 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 export const create = () => {
 
-    const requestListener = (request, response) => {};
+    const requestListener = (request, response) => {
+
+        const folderStatic = path.join(process.cwd(), 'static');
+
+        const promise = (async () => {
+
+            response.setHeader('Content-Type', 'text/html');
+
+            const pathRootDocument = path.join(folderStatic, 'index.html');
+            const dataRootDocument = await fs.readFile(pathRootDocument);
+            response.write(dataRootDocument);
+
+        })();
+
+        promise.finally(() => {
+
+            response.end();
+
+        });
+
+    };
 
     return Object.freeze({
         requestListener,
